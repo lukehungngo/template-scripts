5bae4d62e26664d1be816345f641fd89e2f87999
hungngo <qhung.bm0409@gmail.com>
2023-09-12 22:29:00 +0700

    init

---
+++ clone_golang_sc.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+GITHUB_URL="https://github.com/lukehungngo/template-golang.git"
+
+DEST_DIR=$1
+
+if [ -z "$DEST_DIR" ]; then
+	echo "Error: DEST_DIR is empty. Please specify a destination directory"
+	exit 1
+fi
+
+git clone $GITHUB_URL $DEST_DIR
+
+if [ $? -ne 0]; then
+	echo "Error: Failed to clone repository"
+	exit 1
+fi
+
+rm -rf "$DEST_DIR/git"
+
+cd $DEST_DIR
+
+git init
